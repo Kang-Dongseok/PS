28cde46cf18f0374123c703e30cee9aeaca69e05
강동석 <kds2048@naver.com>
2021-08-02 23:28:13 +0900

    ADD Baekjoon_2231

--- src/baekjoon/Baekjoon_2231.java
+++ src/baekjoon/Baekjoon_2231.java
@@ -1,10 +1,39 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Baekjoon_2231 {
-미해결
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int n_length = isLength(n);  // n의 자릿수
+		int answer = 0;
+		for(int i=n-1;i>=n-(9*n_length);i--) {
+			if(func(i)==n) {
+				answer = i;
+			}
+		}
+		System.out.println(answer);
 	}
-
+	public static int isLength(int n) {
+		int length = 1;
+		while(n/10>0) {  // n의 자릿수 구하기
+			n /= 10;
+			length++;
+		}
+		return length;
+	}
+	public static int func(int n) {  // 자릿수의 합을 다 더한 수
+		int sum = n;
+		while(n/10>0) {
+			sum += n%10;
+			n /= 10;
+		}
+		sum += n;
+		return sum;
+	}
 }
