7444b574de95ffdd5f6499d431a7551a1b68ea06
강동석 <kds2048@naver.com>
2021-08-04 00:45:12 +0900

    2021-08-04

---
+++ src/baekjoon/Baekjoon_18870.java
@@ -0,0 +1,56 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+// 좌표 압축
+/*
+ * 값이 같을 떄 결과가 같으면 Map<Key, Value>을 떠올리자!
+ */
+public class Baekjoon_18870 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		// 배열 생성 후 대입
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		// 배열 정렬
+		int[] sortedArr = arr.clone();
+		Arrays.sort(sortedArr);
+		
+		int size = sortedArr.length;
+		int flag = sortedArr[size-1];
+		int cnt = 0;
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		if(flag==sortedArr[0]) { // 모든 값이 같으면
+			map.put(flag,cnt++);
+		}else {
+			for(int i=0;i<size;i++) {
+				if(flag!=sortedArr[i]) {
+					flag = sortedArr[i];
+					map.put(flag,cnt++);
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i=0,len=arr.length; i<len;i++) {
+			if(i==len-1) {
+				sb.append(map.get(arr[i]));
+				break;
+			}
+			sb.append(map.get(arr[i])+" ");
+		}
+		System.out.print(sb.toString());
+	}
+
+}
