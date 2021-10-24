93855c23a0c6770eca1ce631de8324144d792fdf
강동석 <kds2048@naver.com>
2021-10-24 23:11:18 +0900

    UPDATE 2021-10-24

---
+++ src/baekjoon/Baekjoon_5337.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+// 웰컴
+public class Baekjoon_5337 {
+	public static void main(String[] args) {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
