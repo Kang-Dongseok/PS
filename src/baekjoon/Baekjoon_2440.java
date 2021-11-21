404dd957345fa18ffd8bd1b053eed829bfe3ad5c
강동석 <kds2048@naver.com>
2021-11-21 17:14:25 +0900

    UPDATE 2021-11-21

---
+++ src/baekjoon/Baekjoon_2440.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 별 찍기 - 3
+public class Baekjoon_2440 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i=N; i>=1; i--) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+}
