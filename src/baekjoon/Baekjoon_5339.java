669fe3613b44d08a8865005ee7bb32df2a2455c4
강동석 <kds2048@naver.com>
2022-05-28 23:06:09 +0900

    UPDATE 2022-05-28

---
+++ src/baekjoon/Baekjoon_5339.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+// 콜센터
+public class Baekjoon_5339 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
