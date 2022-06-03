af8c2aa4e6f6989608d9d1020a14c8f948d5fa75
강동석 <kds2048@naver.com>
2022-06-03 09:31:21 +0900

    UPDATE 2022-06-03

---
+++ src/baekjoon/Baekjoon_25083.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.IOException;
+
+// 새싹
+public class Baekjoon_25083 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
