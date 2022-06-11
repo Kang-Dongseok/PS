2626567c6d99d1a2c4f5fad5d650dc1d688b82b8
강동석 <kds2048@naver.com>
2022-06-11 21:27:44 +0900

    UPDATE 2022-06-11

---
+++ src/baekjoon/Baekjoon_2234.java
@@ -0,0 +1,111 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+// 성곽
+public class Baekjoon_2234 {
+
+	static int N,M;
+	static int[][] map;
+	static boolean[][][] path;
+	static Map<Integer, Integer> cntMap;
+	static int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}}; // 상우하좌
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		map = new int[M][N];
+		path = new boolean[M][N][4];
+		cntMap = new HashMap<Integer, Integer>();
+		for(int i=0; i<M; ++i) {
+			str = br.readLine().split(" ");
+			for(int j=0; j<N; ++j) {
+				int n = Integer.parseInt(str[j]);
+				if(n<8) path[i][j][2]=true; // 아래쪽 열려있으면
+				else n -= 8;
+				if(n<4) path[i][j][1]=true; // 우
+				else n -= 4;
+				if(n<2) path[i][j][0]=true; // 상
+				else n -= 2;
+				if(n<1) path[i][j][3]=true; // 좌
+			}
+		}
+		
+		int cnt = 0; // 방의 갯수
+		int max = 0; // 방 하나 최대 넓이
+		int unionMax = 0; // 방 합친 최대 넓이
+		for(int i=0; i<M; ++i) {
+			for(int j=0; j<N; ++j) {
+				if(map[i][j]==0) { // 아직 갯수 표시 안했으면
+					cnt++;
+					bfs(i,j,cnt);
+				}
+			}
+		}
+		for(int key : cntMap.keySet()) {
+			max = Math.max(max, cntMap.get(key));
+		}
+		for(int i=0; i<M; ++i) {
+			for(int j=0; j<N; ++j) {
+				for(int d=0; d<4; ++d) {
+					if(path[i][j][d]) continue; // 뚫려있으면 무시
+					int ni = i+dir[d][0];
+					int nj = j+dir[d][1];
+					if(ni<0 || ni>=M || nj<0 || nj>=N) continue; // 경계체크
+					if(map[i][j]!=map[ni][nj]) { // 서로 다른 구역이면
+						int total = cntMap.get(map[i][j])+cntMap.get(map[ni][nj]);
+						unionMax = Math.max(unionMax, total);
+					}
+				}
+			}
+		}
+		for(int[] ar : map) {
+			for(int a : ar) {
+				System.out.print(a+" ");
+			}
+			System.out.println();
+		}
+		System.out.println(cnt);
+		System.out.println(max);
+		System.out.println(unionMax);
+	}
+	
+	public static void bfs(int i, int j, int cnt) {
+		Queue<int[]> q = new LinkedList<int[]>();
+		q.offer(new int[] {i,j});
+		boolean[][] visited = new boolean[M][N];
+		ArrayList<int[]> list = new ArrayList<int[]>();
+		int num = 0; // 칸의 갯수
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int r = cur[0];
+			int c = cur[1];
+			
+			if(visited[r][c]) continue;
+			visited[r][c] = true;
+			list.add(new int[] {r,c});
+			num++;
+			
+			for(int d=0; d<4; ++d) {
+				if(!path[r][c][d]) continue; // 길이 없으면 무시
+				int nr = r+dir[d][0];
+				int nc = c+dir[d][1];
+				if(nr<0 || nr>=M || nc<0 || nc>=N || visited[nr][nc]) continue;
+				q.offer(new int[] {nr,nc});
+			}
+		}
+		for(int[] cur : list) {
+			map[cur[0]][cur[1]] = cnt;
+		}
+		cntMap.put(cnt, num);
+	}
+}
--- src/baekjoon/Baekjoon_2617.java
+++ src/baekjoon/Baekjoon_2617.java
@@ -4,45 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
-// 미해결
 // 구슬 찾기
 public class Baekjoon_2617 {
 
+	static boolean[] visited;
+	static int[][] total = new int[100][2]; // 작은수와 큰수 갯수 저장
+	static ArrayList<Integer>[] list;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] str = br.readLine().split(" ");
 		int N = Integer.parseInt(str[0]);
 		int M = Integer.parseInt(str[1]);
-		ArrayList<Set<Integer>> before = new ArrayList<Set<Integer>>();
-		ArrayList<Set<Integer>> after = new ArrayList<Set<Integer>>();
-		for(int i=0; i<N; ++i) {
-			before.add(new HashSet<Integer>());
-			after.add(new HashSet<Integer>());
+		list = new ArrayList[100];
+		for(int i=0; i<100; i++) {
+			list[i] = new ArrayList<Integer>();
 		}
+		
 		for(int i=0; i<M; ++i) {
 			str = br.readLine().split(" ");
-			int a = Integer.parseInt(str[0])-1;
-			int b = Integer.parseInt(str[1])-1;
-			before.get(a).add(b);
-			before.get(a).addAll(before.get(b));
-			after.get(b).add(a);
-			after.get(b).addAll(after.get(a));
+			int bigger = Integer.parseInt(str[0])-1;
+			int smaller = Integer.parseInt(str[1])-1;
+			list[bigger].add(smaller);
 		}
 		int answer = 0;
 		for(int i=0; i<N; ++i) {
-//			System.out.println(before.get(i).size());
-//			System.out.println(after.get(i).size());
-			if(before.get(i).size()>=(N+1)/2 || after.get(i).size()>=(N+1)/2) {
-				answer++;
-			}
+			visited = new boolean[100];
+			dfs(i, i); // 모든 숫자마다 자기부터 시작해서 작은 수 검색
+		}
+		for(int i=0; i<N; ++i) {
+			if(total[i][0]>N/2 || total[i][1]>N/2) answer++;
 		}
 		System.out.println(answer);
+	}
+	
+	public static void dfs(int start, int cur) {
+		visited[cur] = true; // 방문표시
 		
-		
-		
-		
+		for(int next : list[cur]) {
+			if(!visited[next]) { // 아직 방문 안했으면
+				total[start][0]++; // start보다 작은 갯수 1 증가
+				total[next][1]++; // next보다 큰 갯수 1 증가
+				dfs(start, next);
+			}
+		}
 	}
 }
