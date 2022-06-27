486866d2a5481b07e494fcff3340758873367a36
강동석 <kds2048@naver.com>
2022-06-27 21:07:36 +0900

    UPDATE 2022-06-27

---
+++ src/baekjoon/Baekjoon_10807.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 개수 세기
+public class Baekjoon_10807 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		String[] str = br.readLine().split(" ");
+		for(int i=0; i<N; ++i) {
+			arr[i] = Integer.parseInt(str[i]);
+		}
+		int v = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		for(int i=0; i<N; ++i) {
+			if(arr[i]==v) cnt++;
+		}
+		System.out.println(cnt);
+	}
+}
