8cc5bdc421fbd043489950bd920fb65024fdc3eb
강동석 <kds2048@naver.com>
2022-07-07 23:45:18 +0900

    UPDATE 2022-07-07

---
+++ src/baekjoon/Baekjoon_17295.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 엔드게임 스포일러
+public class Baekjoon_17295 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Avengers: Endgame");
+	}
+}
