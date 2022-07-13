fe95ac59534b28c653cd872367f1d7a8b975ca1d
강동석 <kds2048@naver.com>
2022-07-13 23:16:39 +0900

    UPDATE 2022-07-13

--- src/baekjoon/Baekjoon_1113.java
+++ src/baekjoon/Baekjoon_1113.java
@@ -3,18 +3,29 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 // 수영장 만들기
+/*
+ * 테두리는 물을 채울 수 없으므로 제외하고
+ * 나머지 칸중에서 높이가 1인 곳 부터 연결되어 있는 구역을 bfs로 전부 탐색해서
+ * 높이를 1씩 증가시켜 나가면 된다.
+ * 구역을 탐색 중에 테두리가 연결되어 있거나, 높이가 낮은 부분이 근접해있으면 채울 수 없으므로 무시한다.
+ */
 public class Baekjoon_1113 {
 	
+	static int N,M,answer;
 	static int[][] map;
 	static boolean[][] visited;
+	static int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] str = br.readLine().split(" ");
-		int N = Integer.parseInt(str[0]);
-		int M = Integer.parseInt(str[1]);
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
 		map = new int[N][M];
 		int top = 1;
 		for(int i=0; i<N; ++i) {
@@ -25,21 +36,53 @@ public class Baekjoon_1113 {
 			}
 		}
 		
-		for(int i=1; i<top; ++i) {
+		for(int h=1; h<top; ++h) {
 			visited = new boolean[N][M];
-			for(int r=1; r<N; ++r) { // 테두리는 어차피 물을 채울 수 없으므로 무시
-				for(int c=1; c<M; ++c) {
-					if(!visited[r][c]) {
-						func(r,c);
+			for(int r=1; r<N-1; ++r) { // 테두리는 어차피 물을 채울 수 없으므로 무시
+				for(int c=1; c<M-1; ++c) {
+					if(!visited[r][c] && map[r][c]==h) { // 아직 물을 안 채웠고, 높이가 h이면
+						fill(r,c,h); // (r,c)에 높이 h이면 물 채우기 시도
 					}
 				}
 			}
 		}
-		
-		
+		System.out.println(answer);
 	}
 	
-	public static void func(int r, int c) {
-		
+	public static void fill(int i, int j, int h) { // (r,c)에서 높이 h에 물을 채울 수 있는지 확인
+		Queue<int[]> q = new LinkedList<int[]>();
+		ArrayList<int[]> list = new ArrayList<int[]>();
+		boolean flag = false; // 채울 수 있는지 확인, false면 가능 true면 불가능
+		q.offer(new int[] {i,j});
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int r = cur[0];
+			int c = cur[1];
+			
+			if(r==0 || c==0 || r==N-1 || c==M-1) { // 테두리이면 물 채우기 불가
+				flag = true; // 채우기 불가능 체크
+				continue;
+			}
+			if(visited[r][c]) continue; // 방문체크
+			visited[r][c] = true;
+			list.add(new int[] {r,c});
+			for(int d=0; d<4; ++d) {
+				int nr = r+dir[d][0];
+				int nc = c+dir[d][1];
+				if(nr<0 || nr>=N || nc<0 || nc>=M || visited[nr][nc]) continue; // 경계 및 방문
+				if(map[r][c]>map[nr][nc]) { // 옆칸이 높이가 낮으면
+					flag = true; // 채우기 불가능
+				}
+				if(map[r][c]==map[nr][nc]) { // 높이가 같은 칸만 추가
+					q.offer(new int[] {nr,nc});
+				}
+			}
+		}
+		if(!flag) {
+			for(int[] cur : list) {
+				map[cur[0]][cur[1]]++; // 물 높이 1씩 채우기
+				answer++; // 정답 증가
+			}
+		}
 	}
 }
---
+++ src/baekjoon/Baekjoon_1202.java
@@ -0,0 +1,65 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+// 보석 도둑
+public class Baekjoon_1202 {
+
+	static class Jewlry implements Comparable<Jewlry> {
+		
+		int w,cost;
+		
+		public Jewlry(int w, int cost) {
+			this.w = w;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Jewlry o) {
+			if(this.w==o.w) { // 무게가 같으면
+				return Double.compare(o.cost, this.cost); // 가격 내림차순
+			}
+			return Integer.compare(this.w, o.w); // 무게가 다르면 무게 오름차순
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		int N = Integer.parseInt(str[0]);
+		int K = Integer.parseInt(str[1]);
+		PriorityQueue<Jewlry> jewlryPQ = new PriorityQueue<Jewlry>();
+		for(int i=0; i<N; ++i) {
+			str = br.readLine().split(" ");
+			int w = Integer.parseInt(str[0]);
+			int cost = Integer.parseInt(str[1]);
+			jewlryPQ.offer(new Jewlry(w, cost));
+		}
+		int[] bags = new int[K];
+		for(int i=0; i<K; ++i) {
+			bags[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(bags); // 가방 무게 오름차순
+		
+		PriorityQueue<Integer> costPQ = new PriorityQueue<Integer>(Comparator.reverseOrder()); // 보석 가격 내림차순 정렬
+		
+		long answer = 0; // long 범위 주의!
+		for(int i=0; i<K; ++i) {
+			
+			while(!jewlryPQ.isEmpty() && jewlryPQ.peek().w <= bags[i]) { // 보석이 아직 남아있고, 보석을 가방에 담을 수 있으면
+				costPQ.offer(jewlryPQ.poll().cost); // 보석의 가격을 pq에 담기
+			}
+			
+			if(!costPQ.isEmpty()) {
+				answer += costPQ.poll(); // 가장 비싼 가격 더하기
+			}
+		}
+		System.out.println(answer);
+	}
+}
