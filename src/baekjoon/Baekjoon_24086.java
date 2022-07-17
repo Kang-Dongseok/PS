839050bad0ff75c3e42ed4e879c29dc735aa7cb5
강동석 <kds2048@naver.com>
2022-07-17 09:10:40 +0900

    UPDATE 2022-07-17

---
+++ src/baekjoon/Baekjoon_24086.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Height
+public class Baekjoon_24086 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		System.out.println(b-a);
+	}
+}
