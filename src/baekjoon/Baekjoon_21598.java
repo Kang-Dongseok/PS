ecf59aef00d44b363ca34ba228aceae68e490dfb
강동석 <kds2048@naver.com>
2022-07-22 19:16:51 +0900

    UPDATE 2022-07-22

---
+++ src/baekjoon/Baekjoon_21598.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// SciComLove
+public class Baekjoon_21598 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("SciComLove");
+		if(Integer.parseInt(br.readLine()) == 2) {
+			System.out.println("SciComLove");
+		}
+	}
+}
